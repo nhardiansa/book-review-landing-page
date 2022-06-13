0874725ef5a2f364d5e44a0e41a6c924a50e98cb
Nabil Hardiansa <nhardiansa@gmail.com>
2022-06-13 21:47:21 +0800

    fix: restore default value

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    fontFamily: {
-      Inter: ["Inter", "sans-serif"],
-      McLaren: ["McLaren", "cursive"],
-      Merriweather: ["Merriweather", "serif"],
-      Poppins: ["Poppins", "sans-serif"],
-    },
-    colors: {
-      primaryBg: "#F9F8F4",
-      primary: "#FBF4E2",
-      secondary: "#B4D51E",
-      primary2: "#FFC735",
-      secondary2: "#463C74",
+    extend: {
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+        McLaren: ["McLaren", "cursive"],
+        Merriweather: ["Merriweather", "serif"],
+        Poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primaryBg: "#F9F8F4",
+        primary: "#FBF4E2",
+        secondary: "#B4D51E",
+        primary2: "#FFC735",
+        secondary2: "#463C74",
+      },
     },
     container: {
       center: true,
